60451f9b7c03591ee4da870335a74da8b5b9b670
Tekla <tekla@Teklas-Mac.local>
2023-11-24 00:27:26 -0800

    Fixed issue

--- Tests/CatFactPackageTests/CatFactPackageTests.swift
+++ Tests/CatFactPackageTests/CatFactPackageTests.swift
@@ -6,7 +6,6 @@ final class CatFactPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CatFactPackage().text, "Hello, World!")
     }
 
     static var allTests = [
